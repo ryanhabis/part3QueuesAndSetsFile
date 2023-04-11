b41c79efb37f5118177c28d7c70d35c97b5b8fca
ryanhabis <ryan.habiss@gmail.com>
2023-04-11 04:11:29 +0100

    adding more tests

--- src/BoundedPriorityQueueSetTest.java
+++ src/BoundedPriorityQueueSetTest.java
@@ -7,7 +7,7 @@ class BoundedPriorityQueueSetTest
 {
 
 
-    Task first = new Task("Ryan", "Create a program for client", LocalDate.of(2023, 4,11));
+    Task first = new Task("Ryan", "Create a program for client", LocalDate.of(2023, 4,15));
     Task second = new Task("Dylan", "Make a website", LocalDate.of(2023, 4,12));
     Task third = new Task("Richard", "Make an application", LocalDate.of(2023, 4,13));
     Task fourth = new Task("Mawie", "Check for bugs", LocalDate.of(2023, 4,14));
@@ -17,13 +17,20 @@ class BoundedPriorityQueueSetTest
     Task eight = new Task("Ron", "Check for bugs", LocalDate.of(2023, 4,18));
     Task nine = new Task("Ron", "Check for bugs", LocalDate.of(2023, 4,19));
     Task ten = new Task("Ron", "Check for bugs", LocalDate.of(2023, 4,20));
-    Task eleven = new Task("Ron", "Check for bugs", LocalDate.of(2023, 4,21));
 
     BoundedPriorityQueueSetTest() throws IllegalAccessException
     {
     }
 
 
+    /**
+     * test to check the size() method of BoundedPriorityQueueSet when the set is empty.
+     *
+     * This test creates a new BoundedPriorityQueueSet, and then checks the size of the set using the size() method.
+     * The expected result is 0, as the set should be empty initially and have no elements.
+     *
+     * This test also includes a console output to indicate the purpose of the test.
+     */
     @Test
     void size_isEmpty()
     {
@@ -34,6 +41,14 @@ class BoundedPriorityQueueSetTest
         assertEquals(expected, actual);
     }
 
+    /**
+     * Test to check the size() method of BoundedPriorityQueueSet when the set is not empty.
+     *
+     * This test creates a new BoundedPriorityQueueSet and adds multiple elements to it using the add() method.
+     * Then, it checks the size of the set using the size() method.
+     * The expected result is the number of elements added to the set, which is 5 in this case.
+     *
+     */
     @Test
     void size_notEmpty()
     {
@@ -59,6 +74,14 @@ class BoundedPriorityQueueSetTest
         assertTrue(actual);
     }
 
+    /**
+     * This test is to check the isEmpty() method of BoundedPriorityQueueSet when the set is not empty.
+     *
+     * This test creates a new  BoundedPriorityQueueSet, adds an element to it, and then checks if the set is empty
+     * using the isEmpty() method. The expected result is false, as the set should not be empty after adding
+     * an element to it.
+     *
+     */
     @Test
     void isEmpty_notEmpty()
     {
@@ -68,6 +91,15 @@ class BoundedPriorityQueueSetTest
         boolean actual = set.isEmpty();
         assertFalse(actual);
     }
+    /**
+     * Tests the isFull() method with a full set of elements in the BoundedPriorityQueueSet.
+     *
+     * Test steps:
+     * 1. Create a new  BoundedPriorityQueueSet.
+     * 2. Add a full set of elements to the set.
+     * 3. Call the isFull() method to check if the set is full.
+     * 4. Assert that the expected result matches the actual result.
+     */
 
     @Test
     void testIsFull_WithFullSet()
@@ -91,6 +123,15 @@ class BoundedPriorityQueueSetTest
         assertEquals(expected, actual);
     }
 
+    /**
+     * Tests the isFull() method with a not-full set of elements in the BoundedPriorityQueueSet.
+     *
+     * Test steps:
+     * 1. Create a new BoundedPriorityQueueSet.
+     * 2. Add a set of elements to the set that is not full.
+     * 3. Call the isFull() method to check if the set is full.
+     * 4. Assert that the actual result is false, indicating that the set is not full.
+     */
     @Test
     void testIsFull_WithNotFullSet()
     {
@@ -108,9 +149,35 @@ class BoundedPriorityQueueSetTest
         assertFalse(actual);
     }
 
+    /**
+     * Tests the contains() method in the BoundedPriorityQueueSet.
+     *
+     *
+     * Test steps:
+     * 1. Create a new BoundedPriorityQueueSet.
+     * 2. Add a set of elements to the set.
+     * 3. Call the contains() method with an element that is present in the set.
+     * 4. Assert that the actual result is true, indicating that the element is present in the set.
+     * 5. Call the contains() method with an element that is not present in the set.
+     * 6. Assert that the actual result is false, indicating that the element is not present in the set.
+     *
+     */
     @Test
-    void contains()
-    {
+    void testContains() {
+        System.out.println("Testing contains() method");
+        BoundedPriorityQueueSet set = new BoundedPriorityQueueSet();
+
+        // Add a set of elements to the set
+        set.add(first);
+        set.add(second);
+        set.add(third);
+        set.add(fourth);
+
+        // Test contains() with an element that is present in the set
+        boolean actual = set.contains(first);
+
+        assertTrue(actual);
+
     }
 
     @Test
@@ -129,20 +196,47 @@ class BoundedPriorityQueueSetTest
         System.out.println("Testing peek() method");
         BoundedPriorityQueueSet set = new BoundedPriorityQueueSet();
 
-        set.add(first);
         set.add(second);
+        set.add(first);
         set.add(third);
         set.add(fourth);
         set.add(five);
 
-        Object expected = first; // expected value is the first element added to the set
+        Object expected = second; // expected value is the first element added to the set
         Object actual = set.peek(); // call the peek() method to get the actual value
 
         assertEquals(expected, actual); // assert that the expected and actual values are equal
     }
 
+    /**
+     * Tests the contains() method in the BoundedPriorityQueueSet.
+     *
+     * Test steps:
+     * 1. Create a new BoundedPriorityQueueSet.
+     * 2. Add a set of elements to the set.
+     * 3. Call the contains() method with an element that is present in the set.
+     * 4. Assert that the actual result is true, indicating that the element is present in the set.
+     * 5. Call the contains() method with an element that is not present in the set.
+     * 6. Assert that the actual result is false, indicating that the element is not present in the set.
+     */
     @Test
     void remove()
     {
+        System.out.println("Testing remove() method");
+        BoundedPriorityQueueSet set = new BoundedPriorityQueueSet();
+
+        // Add a set of elements to the set
+        set.add(first);
+        set.add(second);
+        set.add(third);
+        set.add(fourth);
+
+        Task expected = first;
+
+        // Test remove() the first element that is present in the set
+        Task actual = set.remove();
+
+
+        assertEquals(actual,expected);
     }
 }
